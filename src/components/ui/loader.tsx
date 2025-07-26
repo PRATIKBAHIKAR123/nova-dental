8f0e9c7a92d9f1e76739c2e961f5529c63a9c727
Mohammed <mohammedjuzer5252@gmail.com>
2025-07-26 11:35:56 +0530

    added loader

--- src/components/ui/loader.tsx
+++ src/components/ui/loader.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 export default function LoaderProvider({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
@@ -29,16 +30,16 @@ export default function LoaderProvider({ children }: { children: React.ReactNode
 
 const Loader: React.FC = () => (
     <div className='fixed inset-0 z-50 flex items-center justify-center bg-white'>
-    <div className="banter-loader">
-  <div className="banter-loader__box"></div>
-  <div className="banter-loader__box"></div>
-  <div className="banter-loader__box"></div>
-  <div className="banter-loader__box"></div>
-  <div className="banter-loader__box"></div>
-  <div className="banter-loader__box"></div>
-  <div className="banter-loader__box"></div>
-  <div className="banter-loader__box"></div>
-  <div className="banter-loader__box"></div>
-</div>
-</div>
+      <div className="flex flex-col items-center justify-center">
+        <Image
+          src="/Images/heart.gif"
+          alt="Loading..."
+          width={100}
+          height={100}
+          className="w-24 h-24 md:w-32 md:h-32"
+          priority
+        />
+        {/* <p className="mt-4 text-gray-600 font-medium text-lg">Loading...</p> */}
+      </div>
+    </div>
 );
